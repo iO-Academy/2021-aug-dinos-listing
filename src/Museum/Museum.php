a4d4c7240cf1ae44f100d9742a0c23f6c40352c0
Jake Rooms <jakeprooms@outlook.com>
2021-11-08 14:34:32 +0000

    added logic to museum to check for Dinosaur class

--- src/Museum/Museum.php
+++ src/Museum/Museum.php
@@ -2,24 +2,27 @@
 
 namespace DinoApp\Museum;
 
+use DinoApp\Dinosaur\Dinosaur;
+
 class Museum
 {
     public static function displayAllDinos(array $dinos) :string
     {
         $output='';
         foreach ($dinos as $dino){
-            $output .= '<div>';
-            $output .=     '<h2>' . $dino->getSpecies() . '</h2>';
-            $output .=     '<div>';
-            $output .=         '<img src="' . $dino->getImageUrl() .'"/>';
-            $output .=     '</div>';
-            $output .=     '<div>';
-            $output .=         '<div>' . $dino->getFoodType() . '</div>';
-            $output .=         '<button>More</button>';
-            $output .=     '</div>';
-            $output .= '</div>';
+            if($dino instanceof Dinosaur){
+                $output .= '<div>';
+                $output .=     '<h2>' . $dino->getSpecies() . '</h2>';
+                $output .=     '<div>';
+                $output .=         '<img src="' . $dino->getImageUrl() .'"/>';
+                $output .=     '</div>';
+                $output .=     '<div>';
+                $output .=         '<div>' . $dino->getFoodType() . '</div>';
+                $output .=         '<button>More</button>';
+                $output .=     '</div>';
+                $output .= '</div>';
+            }
         }
         return $output;
     }
-
 }
