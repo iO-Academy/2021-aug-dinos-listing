b70d48c564d8c1c803e868b42248443f22824e3f
Eleanor Roberts <eleanorlroberts@gmail.com>
2021-11-08 13:46:03 +0000

    added properties and getters to dinosaur class

--- src/Dinosaur/Dinosaur.php
+++ src/Dinosaur/Dinosaur.php
@@ -4,5 +4,46 @@ namespace DinoApp\Dinosaur;
 
 class Dinosaur
 {
+    protected int $id;
+    protected string $species;
+    protected string $foodType;
+    protected float $height;
+    protected float $weight;
+    protected float $length;
+    protected int $killerRating;
+    protected int $intelligence;
+    protected int $age;
+    protected string $imageUrl;
 
+    // Getters
+    public function getId(): int {
+        return $this->id;
+    }
+    public function getSpecies(): string {
+        return $this->species;
+    }
+    public function getFoodType(): string {
+        return $this->foodType;
+    }
+    public function getHeight(): float {
+        return $this->weight;
+    }
+    public function getWeight(): float {
+        return $this->weight;
+    }
+    public function getLength(): float {
+        return $this->length;
+    }
+    public function getKillerRating(): int {
+        return $this->killerRating;
+    }
+    public function getIntelligence(): int {
+        return $this->intelligence;
+    }
+    public function getAge(): int {
+        return $this->age;
+    }
+    public function getImageUrl(): string {
+        return $this->imageUrl;
+    }
 }
