0b36f67de288409970633be7591bc2f49c78d35a
Jake Rooms <jakeprooms@outlook.com>
2021-11-10 13:52:16 +0000

    made the getSearched Dinos() method in the DinosaurHydrator and unit tested it

--- src/DinosaurHydrator/DinosaurHydrator.php
+++ src/DinosaurHydrator/DinosaurHydrator.php
@@ -30,11 +30,22 @@ class DinosaurHydrator
     public static function getDino(PDO $db, int $id) : Dinosaur
     {
         // Prepares (/stores) the criteria for data we want to retrieve from the db
-        $query = $db->prepare('SELECT `dinos`.`id`, `dinos`.`species`, `dinos`.`foodType`  AS `foodType`, `dinos`.`height`, `dinos`.`weight`, `dinos`.`length`, `dinos`.`killerRating`, `dinos`.`intelligence`, `dinos`.`age`, `dinos`.`imageUrl`, , `foodTypes`.`imageUrl` AS `logoUrl` FROM `dinos` INNER JOIN `foodTypes` ON `dinos`.`foodType` = `foodTypes`.`id` WHERE `id` = :id;');
+        $query = $db->prepare('SELECT `dinos`.`id`, `dinos`.`species`, `foodTypes`.`name`  AS `foodType`, `dinos`.`height`, `dinos`.`weight`, `dinos`.`length`, `dinos`.`killerRating`, `dinos`.`intelligence`, `dinos`.`age`, `dinos`.`imageUrl`, `foodTypes`.`imageUrl` AS `logoUrl` FROM `dinos` INNER JOIN `foodTypes` ON `dinos`.`foodType` = `foodTypes`.`id` WHERE `id` = :id;');
         $query->execute([':id' => $id]);
         // Sets the fetch mode (what format we get the data returned in) to the class of Dinosaur
         $query->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, Dinosaur::class);
         // Fetches all rows which match the criteria (as opposed to fetch() which returns one row)
         return $query->fetch();
+    }
+
+    public static function getSearchedDinos(PDO $db, string $search): Array
+    {
+        // Prepares (/stores) the criteria for data we want to retrieve from the db
+        $query = $db->prepare("SELECT `dinos`.`id`, `dinos`.`species`, `foodTypes`.`name` AS `foodType`, `dinos`.`height`, `dinos`.`weight`, `dinos`.`length`, `dinos`.`killerRating`, `dinos`.`intelligence`, `dinos`.`age`, `dinos`.`imageUrl`, `foodTypes`.`imageUrl` AS `logoUrl` FROM `dinos` INNER JOIN `foodTypes` ON `dinos`.`foodType` = `foodTypes`.`id` WHERE `dinos`.`species` LIKE :search;");
+        $query->execute([':search' => '%' . trim($search) . '%']);
+        // Sets the fetch mode (what format we get the data returned in) to the class of Dinosaur
+        $query->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, Dinosaur::class);
+        // Fetches all rows which match the criteria (as opposed to fetch() which returns one row)
+        return $query->fetchAll();
     }
 }
