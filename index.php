d97e6e521056634f28a187aecb66e569802439d7
jesshamby <jesshamby99@gmail.com>
2021-11-10 14:52:30 +0000

    Merged in s4t2 and got the search functionality working. Created a checkIfSearched and a searchedValue function.

--- index.php
+++ index.php
@@ -7,6 +7,36 @@ require_once 'vendor/autoload.php';
 // Creates a variable which points to the correct database and gives username and password
 $db = new PDO('mysql:host=db;dbname=dinosaurs;', 'root', 'password');
 
+/**
+ * Checks if the form has been submitted and if so returns the dinos from the submitted search. If not, returns all dinos
+ *
+ * @param PDO $db database
+ * @return string of dinos to be displayed
+ */
+function checkIfSearched(PDO $db): string {
+    if (isset($_POST['submit'])) {
+        return Museum::displayAllDinos(DinosaurHydrator::getSearchedDinos($db, $_POST['search']));
+    } else {
+        return Museum::displayAllDinos(DinosaurHydrator::getAllDinos($db));
+    }
+}
+
+/**
+ * Checks if the form has been submitted and if so returns the value the user searched for, otherwise returns search
+ *
+ * @return string
+ */
+function searchedValue(): string {
+    if (isset($_POST['submit'])) {
+        return $_POST['search'];
+    } else {
+        return "search";
+    }
+}
+
+$display = checkIfSearched($db);
+$searchedValue = searchedValue();
+
 ?>
 
 <!DOCTYPE html>
@@ -26,21 +56,16 @@ $db = new PDO('mysql:host=db;dbname=dinosaurs;', 'root', 'password');
 
     <div class="col">
         <div class="row ml-5">
-            <form class="d-flex flex-row" method="post">
-                <input type="search" class="form-control m-2" name="searchInput" id="searchBar" placeholder="<?php if(!isset($_POST['submit'])){echo 'Search';} else {echo $_POST['search'];} ?>">
-                <button type="submit" class="btn m-1">Submit</button>
-                <button type="reset" class="btn m-1">Clear</button>
+            <form class="d-flex flex-row" method="post" action="">
+                <input name="search" type="search" class="form-control m-2" id="search" placeholder="<?php echo $searchedValue; ?>">
+                <input name="submit" type="submit" class="btn m-1" />
+                <input id="reset" name="submit" type="submit" class="btn m-1" value="Clear" />
             </form>
         </div>
 
         <div class="row justify-content-center">
             <?php
-                if(isset($_POST['submit'])) {
-                    echo "<meta http-equiv='refresh' content='0'>";
-                    echo Museum::displayAllDinos(DinosaurHydrator::getSearchedDinos($db));
-                }else {
-                    echo Museum::displayAllDinos(DinosaurHydrator::getAllDinos($db));
-                }
+                echo $display;
             ?>
         </div>
     </div>
