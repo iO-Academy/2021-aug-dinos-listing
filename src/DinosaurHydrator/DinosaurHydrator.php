c3721fa5f370bc6f29095ad0761310176ea0395a
Jake Rooms <jakeprooms@outlook.com>
2021-11-10 14:58:51 +0000

    added a filter parameter and functionality to the getSearchedDinos function

--- src/DinosaurHydrator/DinosaurHydrator.php
+++ src/DinosaurHydrator/DinosaurHydrator.php
@@ -46,11 +46,16 @@ class DinosaurHydrator
      * @param string $search
      * @return Array
      */
-    public static function getSearchedDinos(PDO $db, string $search): Array
+    public static function getSearchedDinos(PDO $db, string $search, string $filter = ''): Array
     {
         // Prepares (/stores) the criteria for data we want to retrieve from the db
-        $query = $db->prepare("SELECT `dinos`.`id`, `dinos`.`species`, `foodTypes`.`name` AS `foodType`, `dinos`.`height`, `dinos`.`weight`, `dinos`.`length`, `dinos`.`killerRating`, `dinos`.`intelligence`, `dinos`.`age`, `dinos`.`imageUrl`, `foodTypes`.`imageUrl` AS `logoUrl` FROM `dinos` INNER JOIN `foodTypes` ON `dinos`.`foodType` = `foodTypes`.`id` WHERE `dinos`.`species` LIKE :search;");
-        $query->execute([':search' => '%' . trim($search) . '%']);
+        if (trim($filter) !== ''){
+            $query = $db->prepare("SELECT `dinos`.`id`, `dinos`.`species`, `foodTypes`.`name` AS `foodType`, `dinos`.`height`, `dinos`.`weight`, `dinos`.`length`, `dinos`.`killerRating`, `dinos`.`intelligence`, `dinos`.`age`, `dinos`.`imageUrl`, `foodTypes`.`imageUrl` AS `logoUrl` FROM `dinos` INNER JOIN `foodTypes` ON `dinos`.`foodType` = `foodTypes`.`id` WHERE `dinos`.`species` LIKE '%saurus%' AND `foodTypes`.`name` = :filter;");
+            $query->execute([':search' => '%' . trim($search) . '%', ':filter' => trim($filter)]);
+        } else {
+            $query = $db->prepare("SELECT `dinos`.`id`, `dinos`.`species`, `foodTypes`.`name` AS `foodType`, `dinos`.`height`, `dinos`.`weight`, `dinos`.`length`, `dinos`.`killerRating`, `dinos`.`intelligence`, `dinos`.`age`, `dinos`.`imageUrl`, `foodTypes`.`imageUrl` AS `logoUrl` FROM `dinos` INNER JOIN `foodTypes` ON `dinos`.`foodType` = `foodTypes`.`id` WHERE `dinos`.`species` LIKE :search;");
+            $query->execute([':search' => '%' . trim($search) . '%']);
+        }
         // Sets the fetch mode (what format we get the data returned in) to the class of Dinosaur
         $query->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, Dinosaur::class);
         // Fetches all rows which match the criteria (as opposed to fetch() which returns one row)
