0eef49b0e5ec64194e165c9a371a3667231d3684
Eleanor Roberts <eleanorlroberts@gmail.com>
2021-11-10 09:21:17 +0000

    changed css, added an a tag to go home

--- displayCase.php
+++ displayCase.php
@@ -12,14 +12,17 @@ require_once 'vendor/autoload.php';
 <head>
     <meta charset="UTF-8">
     <title>Dino Listing</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fruktur&family=Sigmar+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css" />
 </head>
 <body>
-    <nav class="navbar justify-content-center">
-        <img width="150px" alt="Capysaurus Logo" src="Images/CapybarasaurusLogo.gif" />
-        <p class="h1">Capynotasaurus</p>
-    </nav>
+<nav class="navbar sticky-top justify-content-center">
+    <img width="150px" alt="Capysaurus Logo" src="Images/CapybarasaurusLogo.gif" />
+    <h1>Capynotasaurus</h1>
+</nav>
 
     <div class="body row g-0 m-4 justify-content-center">
             <?php
--- index.php
+++ index.php
@@ -12,6 +12,9 @@ require_once 'vendor/autoload.php';
 <head>
     <meta charset="UTF-8">
     <title>Capynotasaurus</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fruktur&family=Sigmar+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css" />
 </head>
